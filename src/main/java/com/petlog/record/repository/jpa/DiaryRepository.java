1b9dc1852d75e0f6fad21f5a2a57a57e85e869e3
JIEUN <163732805+jxieun@users.noreply.github.com>
2026-01-02 18:36:55 +0900

    리캡 생성

--- src/main/java/com/petlog/record/repository/jpa/DiaryRepository.java
+++ src/main/java/com/petlog/record/repository/jpa/DiaryRepository.java
@@ -4,6 +4,7 @@ import com.petlog.record.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -13,4 +14,7 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     // 특정 펫에 대해 작성된 모든 일기 목록 조회
     List<Diary> findAllByPetId(Long petId);
+
+    // AI 리캡 조회를 위해 추가 (필드명이 date일 경우)
+    List<Diary> findAllByPetIdAndDateBetween(Long petId, LocalDate start, LocalDate end);
 }
